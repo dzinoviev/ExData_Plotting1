4a0a761c11eeafa9c63ed46253aafa68d3863d81
Dmitry Zinoviev <dzinoviev@gmail.com>
2015-04-11 16:00:55 -0400

    Comments added.

--- plot1.R
+++ plot1.R
@@ -1,10 +1,15 @@
+# Read the data
 data<-read.csv (file = "household_power_consumption.txt", sep = ';', na.strings = "?")
+
+# Select the portion for analysis
 startDate <- as.Date ("2007-02-01")
 stopDate <- as.Date ("2007-02-02")
 gapData <- subset (data, as.Date (Date, format = "%d/%m/%Y") >= startDate 
                       & as.Date (Date, format = "%d/%m/%Y") <= stopDate, 
           select = Global_active_power)
 
+# Open the PNG device and create a histogram
 png (width = 480, height = 480, filename = "plot1.png")
 hist (gapData$Global_active_power, xlab = "Global Active Power (kilowatts)", col = "#FF2500", 
       main = "Global Active Power")
+dev.off ()
