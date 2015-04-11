166cc37f656f5deb7c9500f04d3e38d53bff4d2d
Dmitry Zinoviev <dzinoviev@gmail.com>
2015-04-11 15:56:32 -0400

    Initial commit.

---
+++ plot1.R
@@ -0,0 +1,10 @@
+data<-read.csv (file = "household_power_consumption.txt", sep = ';', na.strings = "?")
+startDate <- as.Date ("2007-02-01")
+stopDate <- as.Date ("2007-02-02")
+gapData <- subset (data, as.Date (Date, format = "%d/%m/%Y") >= startDate 
+                      & as.Date (Date, format = "%d/%m/%Y") <= stopDate, 
+          select = Global_active_power)
+
+png (width = 480, height = 480, filename = "plot1.png")
+hist (gapData$Global_active_power, xlab = "Global Active Power (kilowatts)", col = "#FF2500", 
+      main = "Global Active Power")
